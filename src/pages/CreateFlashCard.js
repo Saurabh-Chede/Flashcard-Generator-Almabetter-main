f2fabbc265c0a9534eed30a8d0f4583100c70f20
Saurabh-Chede <saurabhchede21@gmail.com>
2024-02-06 21:59:12 +0530

    commit

--- src/pages/CreateFlashCard.js
+++ src/pages/CreateFlashCard.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "react-toastify/dist/ReactToastify.css";
+import { useState ,useSelector } from "react";
 import { Formik, Field, Form, FieldArray } from "formik";
 import "@fortawesome/fontawesome-free/css/all.css";
 import { MdUploadFile } from "react-icons/md";
 
 function CreateFlashCard() {
+
+  
+
+ 
+
   return (
     <div
       className="w-[78%] m-auto mt-2 flex flex-col md:mt-10 "
@@ -26,64 +32,67 @@ function CreateFlashCard() {
           ],
         }}
       >
-        {({}) => (
+        {({ values, handleBlur, handleChange, setFieldValue }) => (
           <Form>
             <div
-              className="bg-white w-[100%] h-[30%] p-[15px] flex flex-col text-left pl-[25px] commonBorder"
+              className="bg-white w-[100%] h-[30%] p-[15px] flex flex-col text-left pl-[25px] rounded-md commonBorder"
               name="createGroupDiv"
             >
-              <div className="flex flex-col ml-2 md:flex-row">
+              <div className="flex flex-col md:flex-row sm:flex-col">
                 <div className="flex flex-col">
-                  <label htmlFor="group_Name"> Create Group </label>
+                  <label
+                    htmlFor="group_Name"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Create Group
+                  </label>
                   <Field
                     name="group_Name"
                     id="group_Name"
                     type="text"
                     placeholder="Group Name"
-                    className=" rounded p-2 text-sm text-gray-900 border border-gray-300 w-full md:w-96 inField bg-gray-50"
+                    className=" rounded p-2 text-sm text-gray-900 border border-gray-300 w-full md:w-72 inField bg-gray-50"
                   ></Field>
                 </div>
 
-                <div>
+                <div className="upload-button">
                   <label
                     htmlFor="upload_Image"
                     class="w-40 h-[3px] cursor-pointer px-2  ml-5 my-3 mt-[24px] p-4  border border-gray-300 flex  items-center justify-center rounded text-sm  "
                   >
-                    {" "}
                     <MdUploadFile className=" text-[2em] text-blue-700" />
                     <span class="font-bold text-blue-700 ">Upload Image</span>
-                  {/* </label> */}
-                  <input className="hidden"
-                         name="upload_Image"
-                         id="upload_Image"
-                         type="file"
-                  ></input>
                   </label>
+                  <input
+                    className="hidden"
+                    name="upload_Image"
+                    id="upload_Image"
+                    type="file"
+                  ></input>
                 </div>
               </div>
 
-              {/* New Div */}
+              {/* Description field started */}
 
-              <div className="flex-col w-[70%] sm:w-[80%] sm:items-center w-100% relative mt-1 ">
-                <label htmlFor="group_Des" className="ml-2.5">
+              <div className="lg:w-[60%] md:w-[60%] sm:w-48  mt-1  ">
+                <label htmlFor="group_Des" className="text-sm font-medium mb-2">
                   Description
                 </label>
                 <textarea
                   name="group_Des"
                   id="group_Des"
                   placeholder="Group Description... "
-                  class=" rounded relative ml-2.5 text-sm pl-2 py-1 mt-1 border border-gray-300 inField bg-gray-50 w-full w-100 h-28 resize-none inField"
+                  class=" rounded-md text-sm p-2 py-2 text-gray-400 mt-1 border border-gray-300 inField bg-gray-50 w-full h-28 resize-none inField"
                 ></textarea>
               </div>
             </div>
 
-            {/* New Div */}
+            {/* term Division started from here */}
 
             <div
-              className="w-[100%] mt-3 pt-1 bg-white  flex flex-col text-left pl-[25px] commonBorder"
+              className="w-[100%] mt-3 pt-1 bg-white  flex flex-col text-left pl-[25px] rounded-md commonBorder"
               name="TermCardfield"
             >
-             
               <div class="flex-col overflow-hidden bg-white rounded-md">
                 <div
                   name="termsDiv"
@@ -125,11 +134,12 @@ function CreateFlashCard() {
                       <span className="flex w-32 p-2 mx-auto font-bold text-blue-700 transition-all ease-in-out border border-blue-700 rounded-lg shadow-md hover:-translate-y-px hover:bg-blue-700 hover:text-white ">
                         Select Image
                       </span>
-                      <input className="hidden"
-                         name="Select image"
-                         id="Select image"
-                         type="file"
-                  ></input>
+                      <input
+                        className="hidden"
+                        name="Select image"
+                        id="Select image"
+                        type="file"
+                      ></input>
                     </label>
                   </div>
                 </div>
@@ -137,7 +147,7 @@ function CreateFlashCard() {
               <div className="inline-block mt-4 mb-6 font-bold text-blue-700 cursor-pointer mx-7">
                 + Add More
               </div>
-             
+              </div>
 
               {/* it's a create button to create a flashcard */}
 
@@ -149,7 +159,7 @@ function CreateFlashCard() {
                   Create
                 </button>
               </div>
-            </div>
+           
           </Form>
         )}
       </Formik>
